faa0f01a2dce43bbdf9faf4ab22f1f6fc81f2fe2
VBel3235 <Vlad95.0753@gmail.com>
2021-09-08 10:32:00 +1000

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Владислав Белов on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
