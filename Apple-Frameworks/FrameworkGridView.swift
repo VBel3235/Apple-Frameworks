5e673ae43f60318db4bdf312fa2fd2464d2d3fe3
VBel3235 <Vlad95.0753@gmail.com>
2021-09-08 12:51:43 +1000

    First commit

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Владислав Белов on 08.09.2021.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    
+    var body: some View {
+        let columns: [GridItem] =
+            Array(repeating: .init(.flexible()), count: 3)
+        ScrollView{
+            LazyVGrid(columns: columns, content: {
+                ForEach(MockData.frameworks) { element in
+                    FrameworkTitleView(framework: element)
+                }
+            })
+        }
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View{
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90, height: 90, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .padding()
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .minimumScaleFactor(0.5)
+                .scaledToFit()
+                .lineLimit(1)
+                
+        }
+    }
+  
+
+}
